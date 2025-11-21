80198f64dabd724715d20a78e479385b3c9e2122
Yuvasri494 <yuvas5466@gmail.com>
2025-11-21 18:52:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,18 @@
-console.log(100);
-console.log('hello friends')
-console.log(100, 'hello', true);
-console.warn('warning');
-console.error('Alert');
-console.table({name: 'yuva', email: 'yuvas5466@gmail.com'})
-console.group('simple');
-console.log(100, 'hello', true);
-console.warn('warning');
-console.error('Alert');
-console.groupEnd();
-const styles = 'padding: 20px; background-color: black; color: white; font-size: 15px';
-console.log('%c Hello World', styles);
+function calculatebmi()
+{
+    
+    let weightinput = prompt("Enter your weight in kg:");
+     let heightinput = prompt("Enter your height in cm:");
 
-let firstName = 'yuva';
-let lastName = 'sri';
+     let weight = parseFloat (weightinput);
+     let heightCM = parseFloat (heightinput)
 
-console.log(firstName,lastName);
- let age = 30;
+     let height = heightCM / 100;
 
- console.log(age);
+     let bmi = weightinput /(height ** 2 );
+     
+     document.getElementById("result").innerHTML = `Your BMI is ${bmi.toFixed(2)};`
 
- age = 20;
-  console.log(age);
-
-  let score;
-  score=40;
-  console.log(score);
-  if(true){
-    score=score + 10;
-  }
-  console.log(score);
-
-
-
-
-const person ={
-    name : 'brad'};
-
-
-
-
-    function greet (){
-      console.log("hello, yuva!");
-    }
-
-    greet();
-
-
-
-    function add(a,b){
-      console.log(a+b);
-    }
-
-    add(5,3);
-    add(10,20);
-    //data types
-    //string
-    const name = 'yuva'
-
-    const output = name
-
-    console.log(name, typeof output);
-    //number
-    const id = 9787
-    const output2 = id
-    console.log(id , typeof output2)
-    // same for bolean,null,undefined,symbol,bigINT
+}
+ 
     
